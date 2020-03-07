1279855837e33d4fc827102506a1617f2d8300e3
Derick Park <derickmpark@gmail.com>
2020-03-07 13:46:07 -0800

    Add README file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const App = () => (
   <div>
-    
+    <div>
+      <h3>Bulbasaur</h3>
+      <img src="http://vignette4.wikia.nocookie.net/nintendo/images/4/43/Bulbasaur.png/revision/latest?cb=20141002083518&path-prefix=en"/>
+      <div>Grass</div>
+    </div>
   </div>
-)
+);
 
+export default App;
